3ff1abd66156a59ad5f47154a6e266dd43f82e16
kystyn <konstantindamaskinskiy@gmail.com>
2021-09-20 15:00:19 +0300

    Commit of deep copy from  repo

---
+++ main.py
@@ -0,0 +1,146 @@
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QFileDialog, QCheckBox
+from math import *
+import matplotlib.pyplot as plt
+import numpy as np
+import design.design as design
+import research
+
+class MyApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.windowStep = 0
+        self.sampleSize = 0
+        self.plotDirName = 'data'
+
+        self.rawData = []
+        self.energyDataToWrite = []
+        self.statsDataToWrite = []
+        self.isShowPlots = False
+
+        self.setupUi(self)
+        self.plotsAreBuiltLabel.setVisible(False)
+        self.energyParamSaved.setVisible(False)
+        self.statsParamSaved.setVisible(False)
+        self.setupSlots()
+
+    def setupOpenFileAction(self):
+        def slot(flag: bool):
+            self.inputFileName = QFileDialog.getOpenFileName(self, 'Открыть файл', 'data', 'TXT files ( *.txt)')[0]
+            self.loadedFileTxt.setText("Загружен файл: " + self.inputFileName)
+            self.energyParamSaved.setVisible(False)
+            self.statsParamSaved.setVisible(False)
+            self.plotsAreBuiltLabel.setVisible(False)
+            self.choosePlotDir.setText('Директорий не выбран')
+
+        self.actionOpen.triggered.connect(slot)
+
+    def setupEnergyParamsAction(self):
+        def slot(flag: bool):
+            writeFileName = QFileDialog.getSaveFileName(self, 'Сохранить как', 'data', 'TXT files ( *.txt)')[0]
+            for startElem, data in self.energyDataToWrite:
+                research.writeToFile(writeFileName + '_energy_' + str(startElem) + '.txt', data,
+                                     'Interval_end_coordinate Events_count Complete_energy Mean_energy Max_energy\n')
+
+            self.energyParamSaved.setVisible(True)
+
+        self.actionSave.triggered.connect(slot)
+        self.energyParamSave.released.connect(lambda: slot(True))
+
+    def setupStatParamsAction(self):
+        def slot():
+            writeFileName = QFileDialog.getSaveFileName(self, 'Сохранить как', 'data', 'TXT files ( *.txt)')[0]
+            research.writeToFile(writeFileName + '_stats.txt', [d[1] for d in self.statsDataToWrite],
+                                 'Interval_end_time Gauss_expected_value Raw_data_argmax FWHM_(FULL_width) '
+                                 'Esum_Gauss Esum_raw Emax_raw Emax_coord_raw\n')
+            self.statsParamSaved.setVisible(True)
+
+        self.statParamSave.released.connect(slot)
+
+    def buildHistogram(self):
+        for startElem, data in self.energyDataToWrite:
+            plt.figure()
+            x = np.asarray([datum.val for datum in data])
+            y = np.asarray([datum.energy for datum in data])
+            plt.plot(x, y, color='blue', label=str(startElem) + '-' + str(startElem + self.windowStep))
+
+            # need normalize
+            nrm = 0
+            argmaxx = np.argmax(y)
+            maxx = x[argmaxx]
+            #miny = np.min(y)
+            #y -= miny
+            x -= maxx
+            for i in range(1, len(y)):
+                nrm += (y[i] + y[i - 1]) * (x[i] - x[i - 1]) / 2
+
+            gaussian, mu, sigma = research.approxGauss(x, y / nrm)
+            max_en_arr = np.array([datum.max_energy for datum in data])
+            max_en, max_en_coord  = np.max(max_en_arr), data[np.argmax(max_en_arr)].max_energy_coord
+            self.statsDataToWrite.append((startElem,
+                (self.rawData[min(len(self.rawData) - 1, startElem + self.windowStep)][0],
+                 mu + maxx, maxx, 2 * sqrt(2 * log(2)) * sigma,
+                 gaussian(x[argmaxx]) * nrm, y[argmaxx], max_en, max_en_coord)))
+
+            x = np.linspace(data[0].val, data[-1].val, 300)
+            plt.plot(x, [gaussian(z - maxx) * nrm for z in x], color='red', label='Аппроксимация Гаусс')
+
+            plt.legend()
+
+
+            plt.xlabel('Координата')
+            plt.ylabel('Энергия')
+            plt.title('Начальное событие: ' + str(startElem))
+            plt.savefig(self.plotDirName + '/plot_' + str(startElem) + '.png', dpi=600)
+            if self.showPlots.isChecked():
+                plt.show()
+
+    def setupBuildButton(self):
+        def slot():
+            self.rawData = research.readArray(self.inputFileName, '\t')
+            self.energyDataToWrite = []
+
+            self.sampleSize = int(self.sampleSizeLE.text())
+            self.windowStep = int(self.windowStepLE.text())
+
+            for startElem in range(0, len(self.rawData), self.windowStep):
+                self.energyDataToWrite.append\
+                    ((startElem, research.evalDistrSum(self.rawData[startElem: min(startElem + self.sampleSize, len(self.rawData) - 1)],
+                                               1, float(self.distrStepLE.text()))))
+
+            self.buildHistogram()
+            self.plotsAreBuiltLabel.setVisible(True)
+
+        self.buildButton.released.connect(slot)
+
+    def setupPlotDirectory(self):
+        def slot():
+            self.plotDirName = QFileDialog.getExistingDirectory(self, 'Сохранить как', 'data')
+            self.choosePlotDir.setText('Выбран директорий: ' + self.plotDirName)
+        self.plotDir.released.connect(slot)
+
+    def setupShowPlots(self):
+        def slot(state):
+            self.isShowPlots = (state == 1)
+        self.showPlots.stateChanged.connect(slot)
+
+    def setupSlots(self):
+        self.setupOpenFileAction()
+        self.setupEnergyParamsAction()
+        self.setupStatParamsAction()
+        self.setupBuildButton()
+        self.setupPlotDirectory()
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyApp()
+    window.resize(app.screens()[0].size())
+    window.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
---
+++ research.py
@@ -0,0 +1,208 @@
+import numpy as np
+import gradient
+import math
+
+class DistrData:
+    def __init__(self, val, count, energy, mid_energy, max_energy, max_energy_coord):
+        self.val = val
+        self.count = count
+        self.energy = energy
+        self.mid_energy = mid_energy
+        self.max_energy = max_energy
+        self.max_energy_coord = max_energy_coord
+
+class Criteria:
+    def __init__(self, _num, _left, _right):
+        self.columnNum = _num
+        self.left = _left
+        self.right = _right
+
+
+def readArray(fileName, splitter):
+    res = [[]]
+    f = open(fileName, 'rt')
+    for s in f:
+        try:
+            res.append([float(n) for n in s.split(splitter)])
+        except:
+            if len(res) > 0:
+                res.pop()
+    f.close()
+    return res
+
+
+def select(array, criteries):
+    i = 0
+    while i < len(array):
+        broken = False
+        for criteria in criteries:
+            if criteria.columnNum > len(array[i]) - 1:
+                array.remove(array[i])
+                broken = True
+                break
+            if array[i][criteria.columnNum] < criteria.left or \
+                    array[i][criteria.columnNum] > criteria.right:
+                array.remove(array[i])
+                broken = True
+                break
+        if not broken:
+            i = i + 1
+    return array
+
+
+def readCriteries():
+    criteries = []
+    while True:
+        print('Введите номер столбца, нижний и верхние пределы для отбора или нажмите enter для завершения:', end=' ')
+        s = input().split()
+        if len(s) == 0:
+            break
+        criteries.append(Criteria(int(s[0]) - 1, float(s[1]), float(s[2])))
+    print('Ввод критериев завершён!')
+    return criteries
+
+
+def writeToFile(fileName, array, header=''):
+    f = open(fileName, 'w')
+
+    f.write(header)
+    for s in array:
+        iterator = s
+
+        if type(s) == DistrData:
+            iterator = (s.val, s.count, s.energy, s.mid_energy, s.max_energy)
+
+        for st in iterator:
+            f.write('%.6f ' % st)
+        f.write('\n')
+
+    f.close()
+
+
+def makeDistribution( array, column, step ):
+    array.sort(key=lambda row: row[column])
+    distr = []
+    curCount = 0
+    curVal = array[0][column]
+
+    arrIdx = 0
+
+    while arrIdx < len(array):
+        if curVal <= array[arrIdx][column] < curVal + step:
+            curCount += 1
+            arrIdx += 1
+        else:
+            distr.append((curVal, curCount))
+            curVal += step
+            curCount = 0
+    distr.append((curVal, curCount))
+    return distr
+
+
+def evalDistrSum(array, column, step):
+    array.sort(key=lambda row: row[column])
+    distr = []
+    curCount = 0
+    curVal = array[0][column]
+    curEnergy = 0
+    maxEnergy = 0
+    maxEnergyCoord = 0
+
+    arrIdx = 0
+
+    while arrIdx < len(array):
+        if curVal <= array[arrIdx][column] < curVal + step:
+            curEnergy += array[arrIdx][2]
+            if array[arrIdx][2] > maxEnergy:
+                maxEnergy = array[arrIdx][2]
+                maxEnergyCoord = array[arrIdx][1]
+            
+            curCount += 1
+            arrIdx += 1
+        else:
+            curVal += step
+            distr.append(DistrData(curVal, curCount, curEnergy, (curEnergy / curCount if curCount > 0 else 0), maxEnergy, maxEnergyCoord))
+            curCount = 0
+            curEnergy = 0
+            maxEnergy = 0
+            maxEnergyCoord = 0
+    distr.append(DistrData(curVal + step, curCount, curEnergy, (curEnergy / curCount if curCount > 0 else 0), maxEnergy, maxEnergyCoord))
+    return distr
+
+
+def readDistrInput(arr):
+    while True:
+        print('Введите номер столбца, по которому требуется построить распределение, и шаг, либо enter для завершения:', end=' ')
+        s = input().split()
+        if len(s) == 0:
+            break
+        column = int(s[0]) - 1
+        step = float(s[1])
+        print('Введите имя выходного файла для распределения:', end=' ')
+        writeToFile(input(), makeDistribution(arr, column, step))
+
+
+def distr():
+    print('Введите имя входного файла:', end=' ')
+    fileName = input()
+    arr = readArray(fileName, ',')
+
+    criteries = readCriteries()
+
+    arr = select(arr, criteries)
+    print('Введите имя выходного файла для селекции:', end=' ')
+    writeToFile(input(), arr)
+
+    
+    readDistrInput(arr)
+
+
+def energySum():
+    print('Введите имя входного файла:', end=' ')
+    fileName = input()
+    arr = readArray(fileName, '\t')
+
+    print('Введите шаг:', end=' ')
+    arr = evalDistrSum(arr, 1, float(input()))
+
+    print('Введите имя выходного файла для селекции:', end=' ')
+    writeToFile(input(), arr)
+
+
+def approxGauss(x, y, nrm=1.0):
+    n = len(x)
+
+    def gaussian(x, mu, sigma):
+        return nrm / (sigma * math.sqrt(2 * math.pi)) * math.exp(-((x - mu) ** 2) / (2 * sigma ** 2))
+
+    def func(z):
+        mu, sigma = z[0], z[1]
+        res = 0
+
+        for i in range(n):
+            res += (gaussian(x[i], mu, sigma) - y[i]) ** 2
+
+        return res
+
+    def func_gradient(z):
+        mu, sigma = z[0], z[1]
+
+        res_mu = 0
+        res_sigma = 0
+
+        # mu
+        for i in range(n):
+            res_mu += 2 * (gaussian(x[i], mu, sigma) - y[i]) * \
+                   gaussian(x[i], mu, sigma) * (x[i] - mu) / (sigma ** 2)
+
+        # sigma
+        for i in range(n):
+            res_sigma += 2 * (gaussian(x[i], mu, sigma) - y[i]) * nrm / math.sqrt(2 * math.pi) *\
+                         ((-sigma ** 2 + (x[i] - mu) ** 2) / (sigma ** 4)) *\
+                         math.exp(-((x[i] - mu) ** 2) / (2 * sigma ** 2))
+
+        return np.asarray([res_mu, res_sigma])
+
+    solution = gradient.optimal_gradient(func, func_gradient, np.asarray([0, 1]), 1e-5)
+
+    return (lambda x: gaussian(x, solution[0][0], solution[0][1])), solution[0][0], solution[0][1]
